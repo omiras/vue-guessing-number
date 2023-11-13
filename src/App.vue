de00975852b4424a1d6f736cad4982a15ad63b0f
omiras <oscar.miras@gmail.com>
2023-11-13 13:56:18 +0100

    soltion class

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,50 @@
 
 import { ref } from 'vue';
 
+// Número a adivinar
+const randomNumber = ref(parseInt(Math.random() * 100) + 1)
+
+// Variable de estado nos determina si estamos jugando o no
 const isPlaying = ref(false)
+
+// variable para almacenar todos los números que el jugador va probando
+const previousGuesses = ref([])
+
+// Número que ha escrito el usuario en el <input>
+const currentGuess = ref("")
 
 const startGame = () => {
   console.log("Game Starts")
+  isPlaying.value = true
 }
 
+const checkGuess = () => {
+  // añadir el número que ha pusto en el input el usuario a la variable previousGuesses
+  previousGuesses.value.push(currentGuess.value)
+  currentGuess.value = ""
+}
+
+// En los ficheros SFC, NO hace falta exportar ninguna variable ni función. Están automáticamente disponibles en todo el <template>
+
+// Reto: al hacer clic en el boton, nos "cambia" de pantalla
+
+/**
+ * 1. Asignar un escuchador de eventos al botón, para que al hacer click ejecute la función startGame
+ * 2. La función startGame debe cambiar la variable de estado isPlaying a true, para indicar que ya estamos jugando
+ * 
+ * Corregir: 13:26
+ */
+
+/**
+ * Reto: al introducir un número en el input y hacer click, debe añadirlo a la lista de números ya probados. Vamos a guardar esos datos en un array y lo vamos a inicilizar como []
+ * 
+ * 1. Añadir la directiva v-model al input. La variable de estado relacionada le podemos llamar currentGuess
+ * 2. Cuando el usuario haga click en Check Guess, tenemos que añadir el número a la variable previousGuesses. Vais a tener que crear una función nueva para gestionar el click de ese botón
+ * 3. Mostrar en la parte adecuada del <template> todos los números que ya ha probado el usuario. NO hace falta computed properties (podríais usarla), simplemente usad la notación mostacho {{ }} para mostrar la variable previousGuessses
+ * 4. BONUS ORIOL: Crear una variable de estado con el número a adivinar entre 1 y 100.
+ * 
+ * Coregir: 13:50
+ */
 
 </script>
 
@@ -20,18 +58,18 @@ const startGame = () => {
       <p>You have 10 attempts to guess the right number.</p>
       <div id="wrapper">
         <label for="guessField">Guess a number</label>
-        <input type="number" id="guessField">
-        <button class="button-check">Check Guess</button>
+        <input v-model="currentGuess" type="number" id="guessField">
+        <button @click="checkGuess" class="button-check">Check Guess</button>
 
         <div class="resultParas">
-          <p>Previous Guesses: <span class="guesses"></span></p>
+          <p>Previous Guesses: <span class="guesses"> {{ previousGuesses.join("-") }}</span></p>
           <p>Guesses Remaining: <span class="lastResult">10</span></p>
           <p class="lowOrHi"></p>
         </div>
       </div>
     </section>
     <section v-else>
-      <button>Start Game</button>
+      <button @click="startGame">Start Game</button>
     </section>
   </main>
 </template>
